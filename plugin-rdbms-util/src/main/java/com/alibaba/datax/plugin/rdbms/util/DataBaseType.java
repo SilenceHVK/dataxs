10a0fc8b3bcada8d2160f88f685dc7e20ba3186e
hvkcoder <hvkcoder@gmail.com>
2021-03-19 12:03:28 +0800

    ✨ 列名关键字转换迁移至 DataBaseType quoteColumnName

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/util/DataBaseType.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/util/DataBaseType.java
@@ -2,6 +2,8 @@ package com.alibaba.datax.plugin.rdbms.util;
 
 import com.alibaba.datax.common.exception.DataXException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,212 +12,210 @@ import java.util.regex.Pattern;
  * <p/>
  */
 public enum DataBaseType {
-    MySql("mysql", "com.mysql.cj.jdbc.Driver"),
-    Tddl("mysql", "com.mysql.cj.jdbc.Driver"),
-    DRDS("drds", "com.mysql.cj.jdbc.Driver"),
-    Oracle("oracle", "oracle.jdbc.OracleDriver"),
-    SQLServer("sqlserver", "com.microsoft.sqlserver.jdbc.SQLServerDriver"),
-    PostgreSQL("postgresql", "org.postgresql.Driver"),
-    RDBMS("rdbms", "com.alibaba.datax.plugin.rdbms.util.DataBaseType"),
-    DB2("db2", "com.ibm.db2.jcc.DB2Driver"),
-    ADS("ads","com.mysql.cj.jdbc.Driver"),
-    ClickHouse("clickhouse", "ru.yandex.clickhouse.ClickHouseDriver"),
-    KingbaseES("kingbasees", "com.kingbase8.Driver"),
-    Oscar("oscar", "com.oscar.Driver");
+	MySql("mysql", "com.mysql.cj.jdbc.Driver"),
+	Tddl("mysql", "com.mysql.cj.jdbc.Driver"),
+	DRDS("drds", "com.mysql.cj.jdbc.Driver"),
+	Oracle("oracle", "oracle.jdbc.OracleDriver"),
+	SQLServer("sqlserver", "com.microsoft.sqlserver.jdbc.SQLServerDriver"),
+	PostgreSQL("postgresql", "org.postgresql.Driver"),
+	RDBMS("rdbms", "com.alibaba.datax.plugin.rdbms.util.DataBaseType"),
+	DB2("db2", "com.ibm.db2.jcc.DB2Driver"),
+	ADS("ads", "com.mysql.cj.jdbc.Driver"),
+	ClickHouse("clickhouse", "ru.yandex.clickhouse.ClickHouseDriver"),
+	KingbaseES("kingbasees", "com.kingbase8.Driver"),
+	Oscar("oscar", "com.oscar.Driver");
 
 
-    private String typeName;
-    private String driverClassName;
+	private String typeName;
+	private String driverClassName;
 
-    DataBaseType(String typeName, String driverClassName) {
-        this.typeName = typeName;
-        this.driverClassName = driverClassName;
-    }
+	DataBaseType(String typeName, String driverClassName) {
+		this.typeName = typeName;
+		this.driverClassName = driverClassName;
+	}
 
-    public String getDriverClassName() {
-        return this.driverClassName;
-    }
+	public String getDriverClassName() {
+		return this.driverClassName;
+	}
 
-    public String appendJDBCSuffixForReader(String jdbc) {
-        String result = jdbc;
-        String suffix = null;
-        switch (this) {
-            case MySql:
-            case DRDS:
-                suffix = "yearIsDateType=false&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false&rewriteBatchedStatements=true";
-                if (jdbc.contains("?")) {
-                    result = jdbc + "&" + suffix;
-                } else {
-                    result = jdbc + "?" + suffix;
-                }
-                break;
-            case Oracle:
-                break;
-            case SQLServer:
-                break;
-            case DB2:
-                break;
-            case PostgreSQL:
-                break;
-            case ClickHouse:
-                break;
-            case RDBMS:
-                break;
-            case KingbaseES:
-                break;
-            case Oscar:
-                break;
-            default:
-                throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type.");
-        }
+	public String appendJDBCSuffixForReader(String jdbc) {
+		String result = jdbc;
+		String suffix = null;
+		switch (this) {
+			case MySql:
+			case DRDS:
+				suffix = "yearIsDateType=false&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false&rewriteBatchedStatements=true";
+				if (jdbc.contains("?")) {
+					result = jdbc + "&" + suffix;
+				} else {
+					result = jdbc + "?" + suffix;
+				}
+				break;
+			case Oracle:
+				break;
+			case SQLServer:
+				break;
+			case DB2:
+				break;
+			case PostgreSQL:
+				break;
+			case ClickHouse:
+				break;
+			case RDBMS:
+				break;
+			case KingbaseES:
+				break;
+			case Oscar:
+				break;
+			default:
+				throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type.");
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public String appendJDBCSuffixForWriter(String jdbc) {
-        String result = jdbc;
-        String suffix = null;
-        switch (this) {
-            case MySql:
-                suffix = "yearIsDateType=false&zeroDateTimeBehavior=convertToNull&rewriteBatchedStatements=true&tinyInt1isBit=false";
-                if (jdbc.contains("?")) {
-                    result = jdbc + "&" + suffix;
-                } else {
-                    result = jdbc + "?" + suffix;
-                }
-                break;
-            case DRDS:
-                suffix = "yearIsDateType=false&zeroDateTimeBehavior=convertToNull";
-                if (jdbc.contains("?")) {
-                    result = jdbc + "&" + suffix;
-                } else {
-                    result = jdbc + "?" + suffix;
-                }
-                break;
-            case Oracle:
-                break;
-            case SQLServer:
-                break;
-            case DB2:
-                break;
-            case PostgreSQL:
-                break;
-            case ClickHouse:
-                break;
-            case RDBMS:
-                break;
-            case KingbaseES:
-                break;
-            case Oscar:
-                break;
-            default:
-                throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type.");
-        }
+	public String appendJDBCSuffixForWriter(String jdbc) {
+		String result = jdbc;
+		String suffix = null;
+		switch (this) {
+			case MySql:
+				suffix = "yearIsDateType=false&zeroDateTimeBehavior=convertToNull&rewriteBatchedStatements=true&tinyInt1isBit=false";
+				if (jdbc.contains("?")) {
+					result = jdbc + "&" + suffix;
+				} else {
+					result = jdbc + "?" + suffix;
+				}
+				break;
+			case DRDS:
+				suffix = "yearIsDateType=false&zeroDateTimeBehavior=convertToNull";
+				if (jdbc.contains("?")) {
+					result = jdbc + "&" + suffix;
+				} else {
+					result = jdbc + "?" + suffix;
+				}
+				break;
+			case Oracle:
+				break;
+			case SQLServer:
+				break;
+			case DB2:
+				break;
+			case PostgreSQL:
+				break;
+			case ClickHouse:
+				break;
+			case RDBMS:
+				break;
+			case KingbaseES:
+				break;
+			case Oscar:
+				break;
+			default:
+				throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type.");
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    public String formatPk(String splitPk) {
-        String result = splitPk;
+	public String formatPk(String splitPk) {
+		String result = splitPk;
 
-        switch (this) {
-            case MySql:
-            case Oracle:
-                if (splitPk.length() >= 2 && splitPk.startsWith("`") && splitPk.endsWith("`")) {
-                    result = splitPk.substring(1, splitPk.length() - 1).toLowerCase();
-                }
-                break;
-            case SQLServer:
-                if (splitPk.length() >= 2 && splitPk.startsWith("[") && splitPk.endsWith("]")) {
-                    result = splitPk.substring(1, splitPk.length() - 1).toLowerCase();
-                }
-                break;
-            case DB2:
-            case PostgreSQL:
-            case KingbaseES:
-            case Oscar:
-                break;
-            default:
-                throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type.");
-        }
+		switch (this) {
+			case MySql:
+			case Oracle:
+				if (splitPk.length() >= 2 && splitPk.startsWith("`") && splitPk.endsWith("`")) {
+					result = splitPk.substring(1, splitPk.length() - 1).toLowerCase();
+				}
+				break;
+			case SQLServer:
+				if (splitPk.length() >= 2 && splitPk.startsWith("[") && splitPk.endsWith("]")) {
+					result = splitPk.substring(1, splitPk.length() - 1).toLowerCase();
+				}
+				break;
+			case DB2:
+			case PostgreSQL:
+			case KingbaseES:
+			case Oscar:
+				break;
+			default:
+				throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type.");
+		}
 
-        return result;
-    }
+		return result;
+	}
 
 
-    public String quoteColumnName(String columnName) {
-        String result = columnName;
+	public String quoteColumnName(String columnName) {
+		columnName = columnName.trim();
+		columnName = columnName.replace("[", "");
+		columnName = columnName.replace("]", "");
+		columnName = columnName.replace("`", "");
+		columnName = columnName.replace("\"", "");
+		columnName = columnName.replace("'", "");
 
-        switch (this) {
-            case MySql:
-                result = "`" + columnName.replace("`", "``") + "`";
-                break;
-            case Oracle:
-                break;
-            case SQLServer:
-                result = "[" + columnName + "]";
-                break;
-            case DB2:
-            case PostgreSQL:
-            case KingbaseES:
-            case Oscar:
-                break;
-            default:
-                throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type");
-        }
+		switch (this) {
+			case MySql:
+				return String.format("`%s`", columnName);
+			case SQLServer:
+				return String.format("[%s]", columnName);
+			case PostgreSQL:
+			case Oracle:
+				return String.format("\"%s\"", columnName);
+			default:
+				return columnName;
+		}
+	}
 
-        return result;
-    }
+	public String quoteTableName(String tableName) {
+		String result = tableName;
 
-    public String quoteTableName(String tableName) {
-        String result = tableName;
+		switch (this) {
+			case MySql:
+				result = "`" + tableName.replace("`", "``") + "`";
+				break;
+			case Oracle:
+				break;
+			case SQLServer:
+				break;
+			case DB2:
+				break;
+			case PostgreSQL:
+				break;
+			case KingbaseES:
+				break;
+			case Oscar:
+				break;
+			default:
+				throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type");
+		}
 
-        switch (this) {
-            case MySql:
-                result = "`" + tableName.replace("`", "``") + "`";
-                break;
-            case Oracle:
-                break;
-            case SQLServer:
-                break;
-            case DB2:
-                break;
-            case PostgreSQL:
-                break;
-            case KingbaseES:
-                break;
-            case Oscar:
-                break;
-            default:
-                throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE, "unsupported database type");
-        }
+		return result;
+	}
 
-        return result;
-    }
+	private static Pattern mysqlPattern = Pattern.compile("jdbc:mysql://(.+):\\d+/.+");
+	private static Pattern oraclePattern = Pattern.compile("jdbc:oracle:thin:@(.+):\\d+:.+");
 
-    private static Pattern mysqlPattern = Pattern.compile("jdbc:mysql://(.+):\\d+/.+");
-    private static Pattern oraclePattern = Pattern.compile("jdbc:oracle:thin:@(.+):\\d+:.+");
+	/**
+	 * 注意：目前只实现了从 mysql/oracle 中识别出ip 信息.未识别到则返回 null.
+	 */
+	public static String parseIpFromJdbcUrl(String jdbcUrl) {
+		Matcher mysql = mysqlPattern.matcher(jdbcUrl);
+		if (mysql.matches()) {
+			return mysql.group(1);
+		}
+		Matcher oracle = oraclePattern.matcher(jdbcUrl);
+		if (oracle.matches()) {
+			return oracle.group(1);
+		}
+		return null;
+	}
 
-    /**
-     * 注意：目前只实现了从 mysql/oracle 中识别出ip 信息.未识别到则返回 null.
-     */
-    public static String parseIpFromJdbcUrl(String jdbcUrl) {
-        Matcher mysql = mysqlPattern.matcher(jdbcUrl);
-        if (mysql.matches()) {
-            return mysql.group(1);
-        }
-        Matcher oracle = oraclePattern.matcher(jdbcUrl);
-        if (oracle.matches()) {
-            return oracle.group(1);
-        }
-        return null;
-    }
-    public String getTypeName() {
-        return typeName;
-    }
+	public String getTypeName() {
+		return typeName;
+	}
 
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
 
 }
--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/util/OriginalConfPretreatmentUtil.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/util/OriginalConfPretreatmentUtil.java
@@ -17,170 +17,150 @@ import java.util.Arrays;
 import java.util.List;
 
 public final class OriginalConfPretreatmentUtil {
-    private static final Logger LOG = LoggerFactory
-            .getLogger(OriginalConfPretreatmentUtil.class);
+	private static final Logger LOG = LoggerFactory
+		.getLogger(OriginalConfPretreatmentUtil.class);
 
-    public static DataBaseType DATABASE_TYPE;
+	public static DataBaseType DATABASE_TYPE;
 
 //    public static void doPretreatment(Configuration originalConfig) {
 //        doPretreatment(originalConfig,null);
 //    }
 
-    public static void doPretreatment(Configuration originalConfig, DataBaseType dataBaseType) {
-        originalConfig.getNecessaryValue(Constant.CONN_MARK, DBUtilErrorCode.REQUIRED_VALUE);
-        doCheckBatchSize(originalConfig);
-        simplifyConf(originalConfig);
-        dealColumnConf(originalConfig);
-    }
+	public static void doPretreatment(Configuration originalConfig, DataBaseType dataBaseType) {
+		originalConfig.getNecessaryValue(Constant.CONN_MARK, DBUtilErrorCode.REQUIRED_VALUE);
+		doCheckBatchSize(originalConfig);
+		simplifyConf(originalConfig);
+		dealColumnConf(originalConfig);
+	}
 
-    public static void doCheckBatchSize(Configuration originalConfig) {
-        // 检查batchSize 配置（选填，如果未填写，则设置为默认值）
-        int batchSize = originalConfig.getInt(Key.BATCH_SIZE, Constant.DEFAULT_BATCH_SIZE);
-        if (batchSize < 1) {
-            throw DataXException.asDataXException(DBUtilErrorCode.ILLEGAL_VALUE, String.format(
-                    "您的batchSize配置有误. 您所配置的写入数据库表的 batchSize:%s 不能小于1. 推荐配置范围为：[100-1000], 该值越大, 内存溢出可能性越大. 请检查您的配置并作出修改.",
-                    batchSize));
-        }
-        originalConfig.set(Key.BATCH_SIZE, batchSize);
-    }
+	public static void doCheckBatchSize(Configuration originalConfig) {
+		// 检查batchSize 配置（选填，如果未填写，则设置为默认值）
+		int batchSize = originalConfig.getInt(Key.BATCH_SIZE, Constant.DEFAULT_BATCH_SIZE);
+		if (batchSize < 1) {
+			throw DataXException.asDataXException(DBUtilErrorCode.ILLEGAL_VALUE, String.format(
+				"您的batchSize配置有误. 您所配置的写入数据库表的 batchSize:%s 不能小于1. 推荐配置范围为：[100-1000], 该值越大, 内存溢出可能性越大. 请检查您的配置并作出修改.",
+				batchSize));
+		}
+		originalConfig.set(Key.BATCH_SIZE, batchSize);
+	}
 
-    public static void simplifyConf(Configuration originalConfig) {
-        List<Object> connections = originalConfig.getList(Constant.CONN_MARK,Object.class);
-        int tableCount = 0;
-        for (int i = 0, len = connections.size(); i < len; i++) {
-            Configuration connConf = Configuration.from(connections.get(i).toString());
-            String jdbcUrl = connConf.getString(Key.JDBC_URL);
-            if (StringUtils.isBlank(jdbcUrl)) {
-                throw DataXException.asDataXException(DBUtilErrorCode.REQUIRED_VALUE, "您未配置的写入数据库表的 jdbcUrl.");
-            }
+	public static void simplifyConf(Configuration originalConfig) {
+		List<Object> connections = originalConfig.getList(Constant.CONN_MARK, Object.class);
+		int tableCount = 0;
+		for (int i = 0, len = connections.size(); i < len; i++) {
+			Configuration connConf = Configuration.from(connections.get(i).toString());
+			String jdbcUrl = connConf.getString(Key.JDBC_URL);
+			if (StringUtils.isBlank(jdbcUrl)) {
+				throw DataXException.asDataXException(DBUtilErrorCode.REQUIRED_VALUE, "您未配置的写入数据库表的 jdbcUrl.");
+			}
 
-            jdbcUrl = DATABASE_TYPE.appendJDBCSuffixForReader(jdbcUrl);
-            originalConfig.set(String.format("%s[%d].%s", Constant.CONN_MARK, i, Key.JDBC_URL),jdbcUrl);
+			jdbcUrl = DATABASE_TYPE.appendJDBCSuffixForReader(jdbcUrl);
+			originalConfig.set(String.format("%s[%d].%s", Constant.CONN_MARK, i, Key.JDBC_URL), jdbcUrl);
 
-            List<String> tables = connConf.getList(Key.TABLE, String.class);
-            if (null == tables || tables.isEmpty()) {
-                throw DataXException.asDataXException(DBUtilErrorCode.REQUIRED_VALUE, "您未配置写入数据库表的表名称. 根据配置DataX找不到您配置的表. 请检查您的配置并作出修改.");
-            }
-            tableCount = tables.size();
-            originalConfig.set(String.format("%s[%d].%s",Constant.CONN_MARK, i, Constant.TABLE_NUMBER_MARK), tables.size());
-        }
-        originalConfig.set(Constant.TABLE_NUMBER_MARK, tableCount);
-    }
+			List<String> tables = connConf.getList(Key.TABLE, String.class);
+			if (null == tables || tables.isEmpty()) {
+				throw DataXException.asDataXException(DBUtilErrorCode.REQUIRED_VALUE, "您未配置写入数据库表的表名称. 根据配置DataX找不到您配置的表. 请检查您的配置并作出修改.");
+			}
+			tableCount = tables.size();
+			originalConfig.set(String.format("%s[%d].%s", Constant.CONN_MARK, i, Constant.TABLE_NUMBER_MARK), tables.size());
+		}
+		originalConfig.set(Constant.TABLE_NUMBER_MARK, tableCount);
+	}
 
-    public static void dealColumnConf(Configuration originalConfig, ConnectionFactory connectionFactory, String oneTable, List<String> userConfiguredColumns, int connectionIndex, int tableIndex) {
-    	boolean isPreCheck = originalConfig.getBool(Key.DRYRUN, false);
-        List<String> allColumns;
-        if (isPreCheck){
-            allColumns = DBUtil.getTableColumnsByConn(DATABASE_TYPE,connectionFactory.getConnecttionWithoutRetry(), oneTable, connectionFactory.getConnectionInfo());
-        } else {
-            allColumns = DBUtil.getTableColumnsByConn(DATABASE_TYPE,connectionFactory.getConnecttion(), oneTable, connectionFactory.getConnectionInfo());
-        }
-        LOG.info("table:[{}] all columns:[\n{}\n].", oneTable, StringUtils.join(allColumns, ","));
-
-
-        if (1 == userConfiguredColumns.size() && "*".equals(userConfiguredColumns.get(0))) {
-            LOG.warn("您的配置文件中的列配置信息存在风险. 因为您配置的写入数据库表的列为*，当您的表字段个数、类型有变动时，可能影响任务正确性甚至会运行出错。请检查您的配置并作出修改.");
-            // 回填其值，需要以 String 的方式转交后续处理
-            originalConfig.set(String.format("%s[%d].%s[%d].%s",Constant.CONN_MARK, connectionIndex, Key.TABLE, tableIndex, Key.COLUMN), convertKeywordCloumns(connectionFactory,allColumns));
-        } else if (userConfiguredColumns.size() > allColumns.size()) {
-            throw DataXException.asDataXException(DBUtilErrorCode.ILLEGAL_VALUE,
-                    String.format("您的配置文件中的列配置信息有误. 因为您所配置的写入数据库表的字段个数:%s 大于目的表的总字段总个数:%s. 请检查您的配置并作出修改.",
-                            userConfiguredColumns.size(), allColumns.size()));
-        } else {
-            // 确保用户配置的 column 不重复
-            ListUtil.makeSureNoValueDuplicate(convertKeywordCloumns(connectionFactory,userConfiguredColumns), false);
-            // 检查列是否都为数据库表中正确的列（通过执行一次 select column from table 进行判断）
-            DBUtil.getColumnMetaData(connectionFactory.getConnecttion(), oneTable,StringUtils.join(userConfiguredColumns, ","));
-        }
-    }
-
-    public static void dealColumnConf(Configuration originalConfig) {
-        List<Object> connections = originalConfig.getList(Constant.CONN_MARK, Object.class);
-        for (int i = 0, len = connections.size(); i < len; i++){
-            Configuration connConf = Configuration.from(connections.get(i).toString());
-            String jdbcUrl = connConf.getString(Key.JDBC_URL);
-            String username = connConf.getString(Key.USERNAME);
-            String password = connConf.getString(Key.PASSWORD);
-            List<JSONObject> tabls = connConf.getList(Key.TABLE, JSONObject.class);
-
-            // 构建 JDBC 连接工厂
-            JdbcConnectionFactory jdbcConnectionFactory = new JdbcConnectionFactory(DATABASE_TYPE, jdbcUrl, username, password);
-            for (int j = 0; j < tabls.size(); j++) {
-                JSONObject table = tabls.get(j);
-                List<String> column = JSONObject.parseArray(table.getString(Key.COLUMN), String.class);
-                dealColumnConf(originalConfig, jdbcConnectionFactory, table.getString(Key.TABLE_NAME), column, i, j);
-                dealWriteMode(originalConfig, i, j, jdbcUrl, DATABASE_TYPE);
-            }
-        }
-    }
-
-    public static void dealWriteMode(Configuration originalConfig, int connectionIndex, int tableIndex, String jdbcUrl,DataBaseType dataBaseType) {
-        // 默认为：insert 方式
-        String writeMode = originalConfig.getString(Key.WRITE_MODE, "INSERT");
-        List<String> columns = originalConfig.getList(String.format("%s[%d].%s[%d].%s", Constant.CONN_MARK, connectionIndex, Key.TABLE, tableIndex, Key.COLUMN), String.class);
-        List<String> valueHolders = new ArrayList<>(columns.size());
-        for (int i = 0; i < columns.size(); i++) {
-            valueHolders.add("?");
-        }
-        // 是否使用强制更新
-        boolean forceUseUpdate = false;
-        //ob10的处理
-        if (dataBaseType == DataBaseType.MySql && isOB10(jdbcUrl)) {
-            forceUseUpdate = true;
-        }
-        String writeDataSqlTemplate = WriterUtil.getWriteTemplate(columns, valueHolders, writeMode,dataBaseType, forceUseUpdate);
-        originalConfig.set(String.format("%s[%d].%s[%d].%s",Constant.CONN_MARK, connectionIndex, Key.TABLE, tableIndex, Constant.INSERT_OR_REPLACE_TEMPLATE_MARK), writeDataSqlTemplate);
-        LOG.info("Write data [\n{}\n], which jdbcUrl like:[{}]", writeDataSqlTemplate, jdbcUrl);
-    }
+	public static void dealColumnConf(Configuration originalConfig, ConnectionFactory connectionFactory, String oneTable, List<String> userConfiguredColumns, int connectionIndex, int tableIndex) {
+		boolean isPreCheck = originalConfig.getBool(Key.DRYRUN, false);
+		List<String> allColumns;
+		if (isPreCheck) {
+			allColumns = DBUtil.getTableColumnsByConn(DATABASE_TYPE, connectionFactory.getConnecttionWithoutRetry(), oneTable, connectionFactory.getConnectionInfo());
+		} else {
+			allColumns = DBUtil.getTableColumnsByConn(DATABASE_TYPE, connectionFactory.getConnecttion(), oneTable, connectionFactory.getConnectionInfo());
+		}
+		LOG.info("table:[{}] all columns:[\n{}\n].", oneTable, StringUtils.join(allColumns, ","));
 
 
-    public static boolean isOB10(String jdbcUrl) {
-        //ob10的处理
-        if (jdbcUrl.startsWith(com.alibaba.datax.plugin.rdbms.writer.Constant.OB10_SPLIT_STRING)) {
-            String[] ss = jdbcUrl.split(com.alibaba.datax.plugin.rdbms.writer.Constant.OB10_SPLIT_STRING_PATTERN);
-            if (ss.length != 3) {
-                throw DataXException
-                        .asDataXException(
-                                DBUtilErrorCode.JDBC_OB10_ADDRESS_ERROR, "JDBC OB10格式错误，请联系askdatax");
-            }
-            return true;
-        }
-        return false;
-    }
+		if (1 == userConfiguredColumns.size() && "*".equals(userConfiguredColumns.get(0))) {
+			LOG.warn("您的配置文件中的列配置信息存在风险. 因为您配置的写入数据库表的列为*，当您的表字段个数、类型有变动时，可能影响任务正确性甚至会运行出错。请检查您的配置并作出修改.");
+			// 回填其值，需要以 String 的方式转交后续处理
+			originalConfig.set(String.format("%s[%d].%s[%d].%s", Constant.CONN_MARK, connectionIndex, Key.TABLE, tableIndex, Key.COLUMN), convertKeywordCloumns(connectionFactory, allColumns));
+		} else if (userConfiguredColumns.size() > allColumns.size()) {
+			throw DataXException.asDataXException(DBUtilErrorCode.ILLEGAL_VALUE,
+				String.format("您的配置文件中的列配置信息有误. 因为您所配置的写入数据库表的字段个数:%s 大于目的表的总字段总个数:%s. 请检查您的配置并作出修改.",
+					userConfiguredColumns.size(), allColumns.size()));
+		} else {
+			// 确保用户配置的 column 不重复
+			ListUtil.makeSureNoValueDuplicate(convertKeywordCloumns(connectionFactory, userConfiguredColumns), false);
+			// 检查列是否都为数据库表中正确的列（通过执行一次 select column from table 进行判断）
+			DBUtil.getColumnMetaData(connectionFactory.getConnecttion(), oneTable, StringUtils.join(userConfiguredColumns, ","));
+		}
+	}
+
+	public static void dealColumnConf(Configuration originalConfig) {
+		List<Object> connections = originalConfig.getList(Constant.CONN_MARK, Object.class);
+		for (int i = 0, len = connections.size(); i < len; i++) {
+			Configuration connConf = Configuration.from(connections.get(i).toString());
+			String jdbcUrl = connConf.getString(Key.JDBC_URL);
+			String username = connConf.getString(Key.USERNAME);
+			String password = connConf.getString(Key.PASSWORD);
+			List<JSONObject> tabls = connConf.getList(Key.TABLE, JSONObject.class);
+
+			// 构建 JDBC 连接工厂
+			JdbcConnectionFactory jdbcConnectionFactory = new JdbcConnectionFactory(DATABASE_TYPE, jdbcUrl, username, password);
+			for (int j = 0; j < tabls.size(); j++) {
+				JSONObject table = tabls.get(j);
+				List<String> column = JSONObject.parseArray(table.getString(Key.COLUMN), String.class);
+				dealColumnConf(originalConfig, jdbcConnectionFactory, table.getString(Key.TABLE_NAME), column, i, j);
+				dealWriteMode(originalConfig, i, j, jdbcUrl, DATABASE_TYPE);
+			}
+		}
+	}
+
+	public static void dealWriteMode(Configuration originalConfig, int connectionIndex, int tableIndex, String jdbcUrl, DataBaseType dataBaseType) {
+		// 默认为：insert 方式
+		String writeMode = originalConfig.getString(Key.WRITE_MODE, "INSERT");
+		List<String> columns = originalConfig.getList(String.format("%s[%d].%s[%d].%s", Constant.CONN_MARK, connectionIndex, Key.TABLE, tableIndex, Key.COLUMN), String.class);
+		List<String> valueHolders = new ArrayList<>(columns.size());
+		for (int i = 0; i < columns.size(); i++) {
+			valueHolders.add("?");
+		}
+		// 是否使用强制更新
+		boolean forceUseUpdate = false;
+		//ob10的处理
+		if (dataBaseType == DataBaseType.MySql && isOB10(jdbcUrl)) {
+			forceUseUpdate = true;
+		}
+		String writeDataSqlTemplate = WriterUtil.getWriteTemplate(columns, valueHolders, writeMode, dataBaseType, forceUseUpdate);
+		originalConfig.set(String.format("%s[%d].%s[%d].%s", Constant.CONN_MARK, connectionIndex, Key.TABLE, tableIndex, Constant.INSERT_OR_REPLACE_TEMPLATE_MARK), writeDataSqlTemplate);
+		LOG.info("Write data [\n{}\n], which jdbcUrl like:[{}]", writeDataSqlTemplate, jdbcUrl);
+	}
+
+
+	public static boolean isOB10(String jdbcUrl) {
+		//ob10的处理
+		if (jdbcUrl.startsWith(com.alibaba.datax.plugin.rdbms.writer.Constant.OB10_SPLIT_STRING)) {
+			String[] ss = jdbcUrl.split(com.alibaba.datax.plugin.rdbms.writer.Constant.OB10_SPLIT_STRING_PATTERN);
+			if (ss.length != 3) {
+				throw DataXException
+					.asDataXException(
+						DBUtilErrorCode.JDBC_OB10_ADDRESS_ERROR, "JDBC OB10格式错误，请联系askdatax");
+			}
+			return true;
+		}
+		return false;
+	}
 
 	/**
 	 * 转换 Cloumn 关键字
+	 *
 	 * @param connectionFactory
 	 * @param columns
 	 * @return
 	 */
-	public static List<String> convertKeywordCloumns(final ConnectionFactory connectionFactory, List<String> columns){
-			List<String> toColumns = new ArrayList<>();
-			columns.forEach(column -> {
-				column = column.trim();
-				column = column.replace("[", "");
-				column = column.replace("]", "");
-				column = column.replace("`", "");
-				column = column.replace("\"", "");
-				column = column.replace("'", "");
-
-				switch (connectionFactory.getDataBaseType()) {
-					case MySql:
-						column = String.format("`%s`", column);
-						break;
-					case SQLServer:
-						column =  String.format("[%s]", column);
-						break;
-					case PostgreSQL:
-					case Oracle:
-						column =  String.format("\"%s\"", column);
-						break;
-					default:
-						break;
-				}
-				toColumns.add(column);
-			});
-			return toColumns;
-		}
+	public static List<String> convertKeywordCloumns(final ConnectionFactory connectionFactory, List<String> columns) {
+		List<String> toColumns = new ArrayList<>();
+		columns.forEach(column -> {
+			toColumns.add(connectionFactory.getDataBaseType().quoteColumnName(column));
+		});
+		return toColumns;
+	}
 
 }
